bf9e6eec4bdf74430abb2c08bd0792fc464d712a
Anushka Chatterjee <252823336+Anushka-git15@users.noreply.github.com>
2026-07-29 15:35:14 +0530

    Update server.js

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 const withDB = async (operation, res) => {
     try {
-        const client = await MongoClient.connect('mongodb+srv://anushkachateerjee15_db_user:anushkachateerjee15@cluster0.1t5pf4r.mongodb.net/');
+        const client = await MongoClient.connect('mongodb+srv://anushkachateerjee15_db_user:anushkachateerjee15@cluster0.1t5pf4r.mongodb.net/my-blog');
 
         const db = client.db('my-blog');
 
